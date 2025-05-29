4b3f7c8d1824b3069927d53a914170076e117ac4
Shravani_Jawalkar <sjawalkar@corelogic.com>
2025-05-29 17:46:28 +0530

    did changes in order service

---
+++ EcommerseApplication/OrderService/src/main/java/org/example/orderservice/Service/OrderService.java
@@ -0,0 +1,153 @@
+package org.example.orderservice.Service;
+
+import org.example.orderservice.Dao.*;
+import org.example.orderservice.Repository.OrderItemsRepository;
+import org.example.orderservice.Repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private OrderItemsRepository orderItemsRepository;
+
+    @Value("${user.service.url}")
+    private String userServiceUrl;
+
+    @Transactional
+    public OrderResponse createOrder(OrderRequest orderRequest) {
+        // Validate if user exists in UserService
+        boolean userExists = validateUser(orderRequest.getUserId());
+        if (!userExists) {
+            throw new IllegalArgumentException("User with ID " + orderRequest.getUserId() + " does not exist");
+        }
+        Optional<Orders> orders = orderRepository.findByUserId(orderRequest.getUserId());
+        OrderItems orderItems = OrderItems.builder()
+                .price(orderRequest.getPrice())
+                .productId(orderRequest.getProductId())
+                .quantity(orderRequest.getQuantity())
+                .orders(orders.orElse(new Orders()))
+                .totalPrice(orderRequest.getPrice().multiply(BigDecimal.valueOf(orderRequest.getQuantity())))
+                .build();
+        orderItemsRepository.save(orderItems);
+        // Create and save the order
+        Orders order = Orders.builder()
+                .userId(orderRequest.getUserId())
+                .totalAmount(orderItems.getTotalPrice())
+                .status(OrderStatus.PENDING)
+                .items(List.of(orderItems))
+                .build();
+        order = orderRepository.save(order);
+        if (order.getId() == null) {
+            throw new HttpClientErrorException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+        OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setOrderId(order.getId());
+        return orderResponse;
+    }
+
+    private boolean validateUser(Long userId) {
+        try {
+            // Example REST call to UserService
+            String url = userServiceUrl + "/api/users/" + userId;
+            ResponseEntity<UserServiceResponse> response = restTemplate.getForEntity(url, UserServiceResponse.class);
+            return response.getStatusCode() == HttpStatus.OK; // User exists if 200 OK is returned.
+        } catch (HttpClientErrorException.NotFound e) {
+            return false; // User does not exist.
+        }
+    }
+
+    public OrderDetails getOrderById(Long orderId) {
+        Optional<Orders> order = orderRepository.findById(orderId);
+        OrderDetails orderResponse = new OrderDetails();
+        if (order.isEmpty()) {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "Order with ID " + orderId + " does not exist");
+        } else {
+            order.ifPresent(orders -> {
+                orderResponse.setOrderId(orders.getId());
+                orderResponse.setUserName(getUserName(orders.getUserId()));
+                orderResponse.setTotalAmount(orders.getTotalAmount());
+                orderResponse.setStatus(String.valueOf(orders.getStatus()));
+                orderResponse.setOrderDetails(orders.getItems().stream()
+                        .map(item -> OrderItemsDetails.builder()
+                                .productId(item.getProductId())
+                                .quantity(item.getQuantity())
+                                .totalPrice(item.getTotalPrice())
+                                .build())
+                        .toList());
+            });
+
+        }
+        return orderResponse;
+    }
+
+    private String getUserName(Long userId) {
+        String url = userServiceUrl + "/api/users/" + userId;
+        ResponseEntity<UserServiceResponse> response = restTemplate.getForEntity(url, UserServiceResponse.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            UserServiceResponse userServiceResponse = response.getBody();
+            if (userServiceResponse != null) {
+                return userServiceResponse.getUserName();
+            } else {
+                throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "User with ID " + userId + " does not exist");
+            }
+        }
+        return response.getStatusCode().toString();
+    }
+
+    @Transactional
+    public String updateOrderStatus(long orderId, String status) {
+        Orders order = orderRepository.findById(orderId).orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Order with ID " + orderId + " does not exist"));
+
+        order.setStatus(OrderStatus.valueOf(status));
+        orderRepository.save(order);
+        return String.format("Order with ID %d has been updated to status %s", orderId, status);
+    }
+
+    public String deleteOrder(long orderId) {
+        Optional<Orders> orders = orderRepository.findById(orderId);
+        if (orders.isEmpty()) {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "Order with ID " + orderId + " does not exist");
+        } else {
+            orderRepository.deleteById(orderId);
+            return String.format("Order with ID %d has been deleted", orderId);
+        }
+    }
+
+    public OrderDetails getOrderByUserId(long userId) {
+        OrderDetails orderResponse = new OrderDetails();
+        orderRepository.findByUserId(userId).ifPresentOrElse(order -> {
+            Orders typeOrder = (Orders) order;
+            orderResponse.setOrderId(typeOrder.getId());
+            orderResponse.setUserName(getUserName(typeOrder.getUserId()));
+            orderResponse.setTotalAmount(typeOrder.getTotalAmount());
+            orderResponse.setStatus(String.valueOf(typeOrder.getStatus()));
+            orderResponse.setOrderDetails(typeOrder.getItems().stream()
+                    .map(item -> OrderItemsDetails.builder()
+                            .productId(item.getProductId())
+                            .quantity(item.getQuantity())
+                            .totalPrice(item.getTotalPrice())
+                            .build())
+                    .toList());
+        }, () -> {
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "Order with User ID " + userId + " does not exist");
+        });
+        return orderResponse;
+    }
+}
--- EcommerseApplication/UserService/src/main/java/org/example/userservice/Service/UsersService.java
+++ EcommerseApplication/UserService/src/main/java/org/example/userservice/Service/UsersService.java
@@ -6,8 +6,11 @@ import org.example.userservice.Dao.UserLoginRequest;
 import org.example.userservice.Dao.Users;
 import org.example.userservice.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Service
 public class UsersService {
@@ -25,47 +28,36 @@ public class UsersService {
         user.setPhone(request.getPhone());
         user.setAddress(request.getAddress());
         userRepository.save(user);
-        UserResponse response = null;
         if (user.getId() == null) {
-            response = new UserResponse();
-            response.setResponseCode("500");
-            return response;
+            throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR,"User is Not Created");
         }
-        response = new UserResponse();
+        UserResponse response = new UserResponse();
         response.setUserId(user.getId());
         response.setUserName(user.getUsername());
-        response.setResponseCode("200");
         return response;
     }
 
     public UserResponse getUserById(Long id) {
         Users user = userRepository.findById(id).orElse(null);
-
-        UserResponse response = null;
         if (user == null) {
-            response = new UserResponse();
-            response.setResponseCode("404");
-            return response;
+            throw new HttpClientErrorException(HttpStatus.NOT_FOUND,"User not found with id: " + id);
         }
-        response = new UserResponse();
+        UserResponse response = new UserResponse();
         response.setUserId(user.getId());
         response.setUserName(user.getUsername());
-        response.setResponseCode("200");
         return response;
     }
+
     @Transactional
     public UserResponse loginUser(UserLoginRequest request) {
         Users user = userRepository.findByUsernameAndPassword(request.getUsername(), request.getPassword());
         UserResponse response = null;
         if (user == null) {
-            response = new UserResponse();
-            response.setResponseCode("404");
-            return response;
+            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST,"Invalid username or password");
         }
         response = new UserResponse();
         response.setUserId(user.getId());
         response.setUserName(user.getUsername());
-        response.setResponseCode("200");
         return response;
     }
 }
